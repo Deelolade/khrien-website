807d1a4bfa503c221ffbcb8d20b4ebbf7c3e299c
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-01-18 10:01:02 +0100

    feature: updated the footer section -i

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import { BsTwitterX } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer className='relative overflow-hidden overscroll-auto max-w-360 mx-auto bg-brandGreen my-6 z-50 shadow-sm rounded-3xl p-8 md:p-16'>
+    <footer className='relative overflow-hidden overscroll-auto max-w-360 mx-auto bg-brandGreen my-6 z-50 shadow-sm rounded-3xl p-8 md:p-20'>
       <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <div className="logo">
           <Image src={logo} width={100} height={100} alt="" />
@@ -21,20 +21,20 @@ const Footer = () => {
         <div className="space-y-4">
           <h1 className='text-2xl font-bold'>Useful Links</h1>
           <ul className='space-y-2'>
-            <li className='text-xl font-bold hover:text-brandPurple transition-all duration-100 ease-in-out'>About Us</li>
-            <li className='text-xl font-bold hover:text-brandPurple transition-all duration-100 ease-in-out'>Services</li>
-            <li className='text-xl font-bold hover:text-brandPurple transition-all duration-100 ease-in-out'>House</li>
-            <li className='text-xl font-bold hover:text-brandPurple transition-all duration-100 ease-in-out'>Academy</li>
-            <li className='text-xl font-bold hover:text-brandPurple transition-all duration-100 ease-in-out'>Contact Us</li>
+            <li className='text-xl font-bold hover:text-brandYellow transition-all duration-100 ease-in-out'>About Us</li>
+            <li className='text-xl font-bold hover:text-brandYellow transition-all duration-100 ease-in-out'>Services</li>
+            <li className='text-xl font-bold hover:text-brandYellow transition-all duration-100 ease-in-out'>House</li>
+            <li className='text-xl font-bold hover:text-brandYellow transition-all duration-100 ease-in-out'>Academy</li>
+            <li className='text-xl font-bold hover:text-brandYellow transition-all duration-100 ease-in-out'>Contact Us</li>
           </ul>
         </div>
         <div className="space-y-4">
           <h1 className='text-2xl font-bold'>Connect with us</h1>
           <div className=" flex space-x-4">
-            <span className='border border-brandGray p-3 rounded-full'><FaFacebook className='scale-150 hover:text-brandPurple transition-all duration-100 ease-in-out' /></span>
-            <span className='border border-brandGray p-3 rounded-full'><FaInstagram className='scale-150 hover:text-brandPurple transition-all duration-100 ease-in-out' /></span>
-            <span className='border border-brandGray p-3 rounded-full'><BsTwitterX className='scale-150 hover:text-brandPurple transition-all duration-100 ease-in-out' /></span>
-            <span className='border border-brandGray p-3 rounded-full'><FaLinkedin className='scale-150 hover:text-brandPurple transition-all duration-100 ease-in-out' /></span>
+            <span className='border border-brandGray p-3 rounded-full'><FaFacebook className='scale-150 hover:text-brandYellow transition-all duration-100 ease-in-out' /></span>
+            <span className='border border-brandGray p-3 rounded-full'><FaInstagram className='scale-150 hover:text-brandYellow transition-all duration-100 ease-in-out' /></span>
+            <span className='border border-brandGray p-3 rounded-full'><BsTwitterX className='scale-150 hover:text-brandYellow transition-all duration-100 ease-in-out' /></span>
+            <span className='border border-brandGray p-3 rounded-full'><FaLinkedin className='scale-150 hover:text-brandYellow transition-all duration-100 ease-in-out' /></span>
           </div>
         </div>
       </div>
