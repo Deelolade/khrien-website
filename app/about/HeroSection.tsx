fa96b78a94a1150fc88d182e540a2ee3b492458e
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-01-20 00:38:59 +0100

    created the herosection on about page

--- app/about/HeroSection.tsx
+++ app/about/HeroSection.tsx
@@ -3,12 +3,19 @@ import heroImage from '@/public/about-bg.jpeg'
 
 const HeroSection = () => {
   return (
-    <>
-      <section className="relative overflow-hidden max-w-360 mx-auto bg-white my-6 mt-[12vh] md:mt-[14vh] shadow-sm rounded-3xl p-8 md:p-20">
-        <Image src={heroImage} alt="" className="w-full h-full object-cover"/>
-      <div className="absolute inset-0 bg-black/50 z-10" />
-      </section>
-    </>
+    <section className="relative overflow-hidden max-w-360 mx-auto my-6 mt-[12vh] md:mt-[14vh] shadow-sm rounded-3xl h-[40vh] md:h-[60vh] flex items-center justify-center">
+      <Image
+        src={heroImage}
+        alt="About Khrien Background"
+        fill
+        className="object-cover"
+        priority
+      />
+      <div className="absolute inset-0 bg-black/40 z-10" />
+      <div className="relative z-20 text-center px-4">
+        <h1 className="text-white text-4xl md:text-7xl font-black uppercase italic">About Khrien</h1>
+      </div>
+    </section>
   );
 };
 export default HeroSection;
