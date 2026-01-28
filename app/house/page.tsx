d4ccb607be932fc6668fcb0ff5b684a87b063183
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-01-28 22:16:34 +0100

    update: created the house page

---
+++ app/house/page.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <main className="px-4 lg:px-9 mt-[12vh] md:mt-[14vh]">
+                {/* <ProductSection /> */}
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default page
