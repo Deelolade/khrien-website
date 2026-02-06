2072b02cbbe826d2d01c4f47ee59a9ed06bcffd0
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-02-06 01:45:35 +0100

    updated: added images to the branding subpage

---
+++ app/services/ServicesSection.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+import { motion } from 'motion/react'
+
+const ServicesSection = () => {
+    const servicesItems = [
+        "/services/services-branding.jpg",
+        "/services/services-branding-two.jpg",
+        "/services/services-branding-three.jpg",
+        "/services/services-branding-four.jpg",
+        "/services/services-branding-five.jpg",
+        "/services/services-branding-six.png"
+    ]
+    return (
+        <section>
+            <div className="max-w-360 mx-auto px-4 md:px-6 lg:py-12 my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
+                {servicesItems.map((item, index) => (
+                    <motion.div
+                        key={index}
+                        initial={{ opacity: 0, y: 30 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: true, margin: "-50px" }}
+                        transition={{ duration: 0.6, delay: index * 0.2 }}
+                    >
+                        <Image src={item} alt="Services" width={300} height={300} className="w-full h-64 md:h-80 lg:h-96 object-cover" />
+                    </motion.div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default ServicesSection
